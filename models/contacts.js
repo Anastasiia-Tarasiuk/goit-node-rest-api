a0d87f519b49639b9b36e0bf4d1512178a43586b
Anastasiia-Tarasiuk <a.yu.tarasiuk@gmail.com>
2022-09-29 14:21:56 +0300

    Adds CRUD methods and validation

--- models/contacts.js
+++ models/contacts.js
@@ -1,84 +1,83 @@
-// const fs = require('fs/promises');
-// const path = require('path');
-// const { v4: uuidv4 } = require('uuid');
-// const id = uuidv4();
 const { Contact } = require('./modelContacts');
-
-
-// const contactsPath = path.resolve(__dirname, 'contacts.json');
 
 const listContacts = async () => {
   try {
     const contacts = await Contact.find({});
-    // console.log(contacts);
     return contacts;
   } catch (error) {
     console.log(error);
   } 
 }
-// listContacts();
 
 const getContactById = async (contactId) => {
-  // try {
-  //   const contacts = JSON.parse(await listContacts());
-  //   const [contact] = contacts.filter(contact => contact.id === contactId);
-  //   return contact;
-  // } catch (error) {
-  //   console.log(error);
-  // }
+  try {
+    const contact = await Contact.findOne({ _id: contactId });
+    return contact;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
+
 const removeContact = async (contactId) => {
-    // try {
-    //   const contacts = JSON.parse(await listContacts());
-    //   const filteredContacts = contacts.filter(contact => contact.id !== contactId);
-      
-    //   await fs.writeFile(contactsPath, JSON.stringify(filteredContacts), 'utf8');
-    //   return contacts;  
-    // } catch (error) {
-    //     console.log(error);    
-    // }
+  try {
+    const contacts = await listContacts();
+    await Contact.findOneAndRemove({ _id: contactId });
+    return contacts;  
+  } catch (error) {
+    console.log(error);    
+  }
 }
 
 const addContact = async (body) => {
-  // try {
-  //   const prevContacts = JSON.parse(await listContacts());
-  //   const { name, email, phone } = body;
-
-  //   const newContact = { id, name, email, phone };
-  //   const allContacts = [...prevContacts, newContact];
-
-  //   await fs.writeFile(contactsPath, JSON.stringify(allContacts), 'utf8');
-  //   return newContact;
-  // } catch (error) {
-  //   console.log(error); 
-  // }
+  try {
+    const { name, email, phone, favorite } = body;
+    const newContact = await Contact.create({
+      name,
+      email,
+      phone,
+      favorite
+    });
+    return newContact;
+  } catch (error) {
+    console.log(error); 
+  }
 }
 
 const updateContact = async (contactId, body) => { 
-  // try {
-  //   const contacts = JSON.parse(await listContacts());
-  //   const { name, email, phone } = body;
+  try {
+    const { name, email, phone, favorite } = body;
+    const updatedContact = {};
 
-  //   contacts.forEach(contact => {
-  //     if (contact.id === contactId) {
-  //       if (name) {
-  //         contact.name = name;
-  //       }
-  //       if (email) {
-  //         contact.email = email;
-  //       }
-  //       if (phone) {
-  //         contact.phone = phone;
-  //       }      
-  //     }
-  //   });
-  //   await fs.writeFile(contactsPath, JSON.stringify(contacts), 'utf8');
-  //   return contacts;
-  // } catch (error) {
-  //   console.log(error); 
-  // }
-
+    if (updatedContact.name !== name) {
+      if (name) {
+        updatedContact.name = name;
+      }
+    }
+    
+    if (updatedContact.email !== email) {
+      if (email) {
+        updatedContact.email = email;
+      }
+    }
+    
+    if (updatedContact.phone !== phone) {
+      if (phone) {
+        updatedContact.phone = phone;
+      }
+    }
+    
+    if (updatedContact.favorite !== favorite) {
+      if (favorite !== undefined) {
+        updatedContact.favorite = favorite;        
+      }
+    }
+    
+    const contact = await Contact.findOneAndUpdate( { _id: contactId }, updatedContact, {new: true} );
+    return contact;
+  } catch (error) {
+    console.log(error); 
+  }
 }
 
 module.exports = {
